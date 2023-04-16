83d3c31ba622d444cd6d2130bb38d4bc3cae8740
Slawomir Kussowski <skussowski@advaoptical.com>
2023-04-16 17:11:21 +0200

    prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,19 @@ import { Observable, delay, of, throwError, timeout } from 'rxjs';
 
 class CustomTimeoutError extends Error {
   constructor() {
-    super('It was too slow');
+    super('Timeout occured');
     this.name = 'CustomTimeoutError';
   }
 }
 
-function timeoutAscending(delay: number) {
+function timeoutAscending(delay: number, retries: number = 1) {
+  if (retries <= 0) {
+    throw new TypeError('Wrong value for retries provided');
+  }
+
   return function <T>(source: Observable<T>): Observable<T> {
     return new Observable(subscriber => {
+      let retried = 0;
       const config = (delayValue: number) => ({
         first: delayValue,
         with: () => throwError(() => new CustomTimeoutError())
@@ -19,14 +24,24 @@ function timeoutAscending(delay: number) {
         next(value: T) { subscriber.next(value); },
         error(error: any) {
           if (error instanceof CustomTimeoutError) {
-            console.log('Timeout error!!!');
+            if (retried < retries) {
+              subscription.unsubscribe();
+              subscription = source
+                .pipe(timeout(config(delay * Math.pow(2, retried++))))
+                .subscribe(observer);
+              console.log(`retry ${retried}. with ${delay * Math.pow(2, retried - 1)}ms`);
+            } else {
+              console.log(`Timeout error after ${retries} retries.`);
+              subscriber.error(error);
+            }
           } else {
+            console.log('other error!');
             subscriber.error(error);
           }
         },
         complete() { subscriber.complete(); }
       }
-      const subscription = source
+      let subscription = source
         .pipe(timeout(config(delay)))
         .subscribe(observer);
 
@@ -43,11 +58,11 @@ function timeoutAscending(delay: number) {
 export class AppComponent {
 
   constructor() {
-    const obs1 = of(1).pipe(delay(500), timeoutAscending(1000));
-    const obs2 = of(2).pipe(delay(1500), timeoutAscending(1000));
+    const obs1 = of(1).pipe(delay(1000), timeoutAscending(300, 2));
+    const obs2 = of(2).pipe(delay(1500), timeoutAscending(300, 4));
 
     obs1.subscribe((val) => console.log(val));
-    obs2.subscribe((val) => console.log(val));
+    setTimeout(() => obs2.subscribe((val) => console.log(val)), 5000);
   }
 
 }
