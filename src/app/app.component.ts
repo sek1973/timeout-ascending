49e79b28528c5724d0d430e0609e2c839659457d
Slawomir Kussowski <skussowski@advaoptical.com>
2023-04-16 15:45:40 +0200

    introduce operator definition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,39 @@
 import { Component } from '@angular/core';
+import { Observable, delay, of, throwError, timeout } from 'rxjs';
+
+class CustomTimeoutError extends Error {
+  constructor() {
+    super('It was too slow');
+    this.name = 'CustomTimeoutError';
+  }
+}
+
+function timeoutAscending(delay: number) {
+  return function <T>(source: Observable<T>): Observable<T> {
+    return new Observable(subscriber => {
+      const config = (delayValue: number) => ({
+        first: delayValue,
+        with: () => throwError(() => new CustomTimeoutError())
+      });
+      const observer = {
+        next(value: T) { subscriber.next(value); },
+        error(error: any) {
+          if (error instanceof CustomTimeoutError) {
+            console.log('Timeout error!!!');
+          } else {
+            subscriber.error(error);
+          }
+        },
+        complete() { subscriber.complete(); }
+      }
+      const subscription = source
+        .pipe(timeout(config(delay)))
+        .subscribe(observer);
+
+      return () => subscription.unsubscribe();
+    });
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +41,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'timeout-ascending';
+
+  constructor() {
+    const obs1 = of(1).pipe(delay(500), timeoutAscending(1000));
+    const obs2 = of(2).pipe(delay(1500), timeoutAscending(1000));
+
+    obs1.subscribe((val) => console.log(val));
+    obs2.subscribe((val) => console.log(val));
+  }
+
 }
